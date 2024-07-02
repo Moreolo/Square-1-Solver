2fb9d773815b97b37c77f8e1ef803f6bf11f0e21
Moritz <coolrizzy100@gmail.com>
2024-07-02 16:38:48 +0200

    Make print of solution look nicer

--- src/main.py
+++ src/main.py
@@ -6,4 +6,8 @@ solver: Solver = Solver()
 # square1, bar_solved = Square1([14, 13, 7, 8, 0, 12, 2, 1, 11, 9, 3, 6, 15, 4, 10, 5]), False
 square1, bar_solved = Square1([2, 7, 13, 15, 5, 1, 14, 0, 11, 12, 3, 9, 10, 6, 8, 4]), False
 print("Scramble: (1,0)/ (3,0)/ (5,-4)/ (-2,-5)/ (-4,-1)/ (3,0)/ (-2,0)/ (0,-3)/ (0,-2)/ (3,0)/ (-1,0)/ (-3,-4)/ (-4,0)/ (0,-2)")
-print(solver.solve(square1, bar_solved))
+solution = solver.solve(square1, bar_solved)
+print("Solution:", solution[0], end='')
+for i in range(1, len(solution)):
+    print('/', solution[i], end='')
+print()
